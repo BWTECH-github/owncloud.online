AustrianDC/core

Adding description to UI test features

tests/ui/features/sharing/federationSharing.feature
@@ -1,4 +1,7 @@
-Feature: FederationSharing
+Feature: FederationSharing with external users
+As a user
+I want to share files with any other user
+So that other users have acess to these files
 
 	Background:
 		Given these users exist:
@@ -48,4 +51,3 @@ Feature: FederationSharing
 		Then the file "renamed file.txt" should be listed
 		But the file "lorem.txt" should not be listed
 		And the file "data.zip" should not be listed
-		

tests/ui/features/sharing/sharing.feature
@@ -1,4 +1,7 @@
-Feature: Sharing
+Feature: Sharing files and folders with internal users
+As a user
+I want to share files and folders with groups and users
+So that those groups and users can acess the files and folders
 
 	Background:
 		Given these users exist:
@@ -63,4 +66,3 @@ Feature: Sharing
 		And I login with username "user1" and password "1234"
 		And I open the folder "simple-folder (2)"
 		Then it should not be possible to delete the file "lorem.txt"
-		

BWTECH-github/owncloud.online

tests/ui/features/sharing/federationSharing.feature
@@ -1,5 +1,8 @@
 @insulated
-Feature: FederationSharing
+Feature: FederationSharing with external users
+As a user
+I want to share files with any other user
+So that other users have access to these files
 
 	Background:
 		Given these users exist:
@@ -50,4 +53,3 @@ Feature: FederationSharing
 		Then the file "renamed file.txt" should be listed
 		But the file "lorem.txt" should not be listed
 		And the file "data.zip" should not be listed
-		

tests/ui/features/sharing/sharing.feature
@@ -1,5 +1,8 @@
 @insulated
-Feature: Sharing
+Feature: Sharing files and folders with internal users
+As a user
+I want to share files and folders with groups and users
+So that those groups and users can access the files and folders
 
 	Background:
 		Given these users exist:
@@ -57,4 +60,3 @@ Feature: Sharing
 		And I login with username "user1" and password "1234"
 		And I open the folder "simple-folder (2)"
 		Then it should not be possible to delete the file "lorem.txt"
-		