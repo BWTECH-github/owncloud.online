LeesinYii/core

Add a new admin section

settings/Controller/LegalSettingsController.php
@@ -1,0 +1,102 @@
+<?php
+/**
+ * @author Viktar Dubiniuk <dubiniuk@owncloud.com>
+ *
+ * @copyright Copyright (c) 2018, ownCloud GmbH
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace OC\Settings\Controller;
+
+use \OCP\AppFramework\Controller;
+use OCP\IRequest;
+use OCP\IL10N;
+use OCP\IConfig;
+
+/**
+ * @package OC\Settings\Controller
+ */
+class LegalSettingsController extends Controller {
+	/**
+	 * @var \OCP\IL10N
+	 */
+	private $l10n;
+
+	/**
+	 * @var \OCP\IConfig
+	 */
+	private $config;
+
+	/**
+	 * @param string $appName
+	 * @param IRequest $request
+	 * @param IL10N $l10n
+	 * @param IConfig $config
+	 */
+	public function __construct($appName,
+								IRequest $request,
+								IL10N $l10n,
+								IConfig $config
+	) {
+		parent::__construct($appName, $request);
+		$this->l10n = $l10n;
+		$this->config = $config;
+	}
+
+	/**
+	 * Store imprint URL
+	 *
+	 * @param string $imprint_url
+	 *
+	 * @return array
+	 */
+	public function setImprintUrl($imprint_url) {
+		$this->config->setSystemValue(
+			'legal.imprint_url',
+			$imprint_url
+		);
+
+		return [
+			'data' =>
+				[
+					'message' => (string) $this->l10n->t('Saved')
+				],
+			'status' => 'success'
+		];
+	}
+
+	/**
+	 * Store privacy policy URL
+	 *
+	 * @param string $privacy_policy
+	 *
+	 * @return array
+	 */
+	public function setPrivacyPolicyUrl($privacy_policy) {
+		$this->config->setSystemValue(
+			'legal.privacy_policy_url',
+			$privacy_policy
+		);
+
+		return [
+			'data' =>
+				[
+					'message' => (string) $this->l10n->t('Saved')
+				],
+			'status' => 'success'
+		];
+	}
+}

settings/js/panels/legal.js
@@ -1,0 +1,20 @@
+$(document).ready(function() {
+	$('#legal_imprint').change(function () {
+		OC.msg.startSaving('#legal_imprint');
+		var post = {
+			imprint_url : $("#legal_imprint").val()
+		};
+		$.post(OC.generateUrl('/settings/admin/legal/imprint'), post, function (data) {
+			OC.msg.finishedSaving('#legal_imprint_msg', data);
+		});
+	});
+	$('#legal_privacy_policy').change(function () {
+		OC.msg.startSaving('#legal_privacy_policy');
+		var post = {
+			imprint : $("#legal_privacy_policy").val()
+		};
+		$.post(OC.generateUrl('/settings/admin/legal/privacypolicy'), post, function (data) {
+			OC.msg.finishedSaving('#legal_privacy_policy_msg', data);
+		});
+	});
+});

tests/Settings/Controller/LegalControllerTest.php
@@ -1,0 +1,82 @@
+<?php
+/**
+ * @author Viktar Dubiniuk <dubiniuk@owncloud.com>
+ *
+ * @copyright Copyright (c) 2018, ownCloud GmbH
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace Tests\Settings\Controller;
+
+use OC\Settings\Application;
+use OC\Settings\Controller\LegalSettingsController;
+use OCP\IConfig;
+
+/**
+ * @package Tests\Settings\Controller
+ */
+class LegalSettingsControllerTest extends \Test\TestCase {
+	/**
+	 * @var \OCP\AppFramework\IAppContainer
+	 */
+	private $container;
+
+	/**
+	 * @var LegalSettingsController
+	 */
+	private $legalSettingsController;
+
+	protected function setUp() {
+		$app = new Application();
+		$this->container = $app->getContainer();
+		$this->container['Config'] = $this->getMockBuilder(IConfig::class)
+			->disableOriginalConstructor()->getMock();
+		$this->container['AppName'] = 'settings';
+		$this->legalSettingsController = $this->container['LegalSettingsController'];
+	}
+
+	/**
+	 * @dataProvider linkData
+	 */
+	public function testSetImprintUrl($link) {
+		$this->container['Config']
+			->expects($this->once())
+			->method('setSystemValue')
+			->with('legal.imprint_url', $link);
+
+		$response = $this->legalSettingsController->setImprintUrl($link);
+		$this->assertArrayHasKey('status', $response);
+	}
+
+	/**
+	 * @dataProvider linkData
+	 */
+	public function testSetPrivacyPolicyUrl($link) {
+		$this->container['Config']
+			->expects($this->once())
+			->method('setSystemValue')
+			->with('legal.privacy_policy_url', $link);
+
+		$response = $this->legalSettingsController->setPrivacyPolicyUrl($link);
+		$this->assertArrayHasKey('status', $response);
+	}
+
+	public function linkData() {
+		return [
+			['https://some.url'],
+		];
+	}
+}

BWTECH-github/owncloud.online

settings/Controller/LegalSettingsController.php
@@ -1,0 +1,104 @@
+<?php
+/**
+ * @author Viktar Dubiniuk <dubiniuk@owncloud.com>
+ *
+ * @copyright Copyright (c) 2018, ownCloud GmbH
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace OC\Settings\Controller;
+
+use \OCP\AppFramework\Controller;
+use OCP\IRequest;
+use OCP\IL10N;
+use OCP\IConfig;
+
+/**
+ * @package OC\Settings\Controller
+ */
+class LegalSettingsController extends Controller {
+	/**
+	 * @var \OCP\IL10N
+	 */
+	private $l10n;
+
+	/**
+	 * @var \OCP\IConfig
+	 */
+	private $config;
+
+	/**
+	 * @param string $appName
+	 * @param IRequest $request
+	 * @param IL10N $l10n
+	 * @param IConfig $config
+	 */
+	public function __construct($appName,
+								IRequest $request,
+								IL10N $l10n,
+								IConfig $config
+	) {
+		parent::__construct($appName, $request);
+		$this->l10n = $l10n;
+		$this->config = $config;
+	}
+
+	/**
+	 * Store imprint URL
+	 *
+	 * @param string $imprintUrl
+	 *
+	 * @return array
+	 */
+	public function setImprintUrl($imprintUrl) {
+		$this->config->setAppValue(
+			'core',
+			'legal.imprint_url',
+			$imprintUrl
+		);
+
+		return [
+			'data' =>
+				[
+					'message' => (string) $this->l10n->t('Saved')
+				],
+			'status' => 'success'
+		];
+	}
+
+	/**
+	 * Store privacy policy URL
+	 *
+	 * @param string $privacyPolicy
+	 *
+	 * @return array
+	 */
+	public function setPrivacyPolicyUrl($privacyPolicy) {
+		$this->config->setAppValue(
+			'core',
+			'legal.privacy_policy_url',
+			$privacyPolicy
+		);
+
+		return [
+			'data' =>
+				[
+					'message' => (string) $this->l10n->t('Saved')
+				],
+			'status' => 'success'
+		];
+	}
+}

settings/js/panels/legal.js
@@ -1,0 +1,20 @@
+$(document).ready(function() {
+	$('#legal_imprint').change(function () {
+		OC.msg.startSaving('#legal_imprint');
+		var post = {
+			imprintUrl : $("#legal_imprint").val()
+		};
+		$.post(OC.generateUrl('/settings/admin/legal/imprint'), post, function (data) {
+			OC.msg.finishedSaving('#legal_imprint_msg', data);
+		});
+	});
+	$('#legal_privacy_policy').change(function () {
+		OC.msg.startSaving('#legal_privacy_policy');
+		var post = {
+			privacyPolicy : $("#legal_privacy_policy").val()
+		};
+		$.post(OC.generateUrl('/settings/admin/legal/privacypolicy'), post, function (data) {
+			OC.msg.finishedSaving('#legal_privacy_policy_msg', data);
+		});
+	});
+});

tests/Settings/Controller/LegalControllerTest.php
@@ -1,0 +1,82 @@
+<?php
+/**
+ * @author Viktar Dubiniuk <dubiniuk@owncloud.com>
+ *
+ * @copyright Copyright (c) 2018, ownCloud GmbH
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace Tests\Settings\Controller;
+
+use OC\Settings\Application;
+use OC\Settings\Controller\LegalSettingsController;
+use OCP\IConfig;
+
+/**
+ * @package Tests\Settings\Controller
+ */
+class LegalSettingsControllerTest extends \Test\TestCase {
+	/**
+	 * @var \OCP\AppFramework\IAppContainer
+	 */
+	private $container;
+
+	/**
+	 * @var LegalSettingsController
+	 */
+	private $legalSettingsController;
+
+	protected function setUp() {
+		$app = new Application();
+		$this->container = $app->getContainer();
+		$this->container['Config'] = $this->getMockBuilder(IConfig::class)
+			->disableOriginalConstructor()->getMock();
+		$this->container['AppName'] = 'settings';
+		$this->legalSettingsController = $this->container['LegalSettingsController'];
+	}
+
+	/**
+	 * @dataProvider linkData
+	 */
+	public function testSetImprintUrl($link) {
+		$this->container['Config']
+			->expects($this->once())
+			->method('setAppValue')
+			->with('core', 'legal.imprint_url', $link);
+
+		$response = $this->legalSettingsController->setImprintUrl($link);
+		$this->assertArrayHasKey('status', $response);
+	}
+
+	/**
+	 * @dataProvider linkData
+	 */
+	public function testSetPrivacyPolicyUrl($link) {
+		$this->container['Config']
+			->expects($this->once())
+			->method('setAppValue')
+			->with('core', 'legal.privacy_policy_url', $link);
+
+		$response = $this->legalSettingsController->setPrivacyPolicyUrl($link);
+		$this->assertArrayHasKey('status', $response);
+	}
+
+	public function linkData() {
+		return [
+			['https://some.url'],
+		];
+	}
+}