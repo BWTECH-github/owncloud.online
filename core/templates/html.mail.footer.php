jzqme/core

Pass links into template via OC_Defaults

core/templates/html.mail.footer.php
@@ -2,3 +2,9 @@
 <?php p($theme->getName()); ?> -
 <?php p($theme->getSlogan()); ?>
 <br><a href="<?php p($theme->getBaseUrl()); ?>"><?php p($theme->getBaseUrl());?></a>
+<?php if ($theme->getImprintUrl() !== ''): ?>
+<br><a href="<?php print_unescaped($theme->getImprintUrl()); ?>"><?php p($l->t('Imprint'));?></a>
+<?php endif; ?>
+<?php if ($theme->getPrivacyPolicyUrl() !== ''): ?>
+	<br><a href="<?php print_unescaped($theme->getPrivacyPolicyUrl()); ?>"><?php p($l->t('Privacy Policy'));?></a>
+<?php endif ?>

core/templates/plain.mail.footer.php
@@ -1,3 +1,11 @@
 --
 <?php p($theme->getName() . ' - ' . $theme->getSlogan()); ?>
-<?php print_unescaped("\n".$theme->getBaseUrl());
+<?php
+print_unescaped("\n" . $theme->getBaseUrl());
+ if ($theme->getImprintUrl() !== '') {
+	print_unescaped("\n" . $theme->getImprintUrl());
+}
+if ($theme->getPrivacyPolicyUrl() !== '') {
+	print_unescaped("\n" . $theme->getPrivacyPolicyUrl());
+}
+?>

BWTECH-github/owncloud.online

core/templates/html.mail.footer.php
@@ -2,3 +2,9 @@
 <?php p($theme->getName()); ?> -
 <?php p($theme->getSlogan()); ?>
 <br><a href="<?php p($theme->getBaseUrl()); ?>"><?php p($theme->getBaseUrl());?></a>
+<?php if ($theme->getImprintUrl() !== ''): ?>
+<br><a href="<?php p($theme->getImprintUrl()); ?>"><?php p($l->t('Imprint'));?></a>
+<?php endif; ?>
+<?php if ($theme->getPrivacyPolicyUrl() !== ''): ?>
+	<br><a href="<?php p($theme->getPrivacyPolicyUrl()); ?>"><?php p($l->t('Privacy Policy'));?></a>
+<?php endif ?>

core/templates/plain.mail.footer.php
@@ -1,3 +1,11 @@
 --
 <?php p($theme->getName() . ' - ' . $theme->getSlogan()); ?>
-<?php print_unescaped("\n".$theme->getBaseUrl());
+<?php
+p("\n" . $theme->getBaseUrl());
+if ($theme->getImprintUrl() !== '') {
+	p("\n" . $theme->getImprintUrl());
+}
+if ($theme->getPrivacyPolicyUrl() !== '') {
+	p("\n" . $theme->getPrivacyPolicyUrl());
+}
+?>