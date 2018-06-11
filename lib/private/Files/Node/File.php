zzwpower/core

Move thumb nailing to the next level ....

lib/private/Files/Node/File.php
@@ -26,9 +26,11 @@
 
 namespace OC\Files\Node;
 
+use OCP\Files\IPreviewNode;
 use OCP\Files\NotPermittedException;
+use OCP\IImage;
 
-class File extends Node implements \OCP\Files\File {
+class File extends Node implements \OCP\Files\File, IPreviewNode {
 	/**
 	 * Creates a Folder that represents a non-existing path
 	 *
@@ -137,5 +139,27 @@ class File extends Node implements \OCP\Files\File {
 	 */
 	public function getChecksum() {
 		return $this->getFileInfo()->getChecksum();
+	}
+
+	/**
+	 * @param array $options
+	 * @return IImage
+	 * @since 10.1.0
+	 */
+	public function getThumbnail($options) {
+		$maxX = array_key_exists('x', $options) ? (int)$_GET['x'] : 32;
+		$maxY = array_key_exists('y', $_GET) ? (int)$_GET['y'] : 32;
+		$scalingUp = array_key_exists('scalingup', $_GET) ? (bool)$_GET['scalingup'] : true;
+		$keepAspect = array_key_exists('a', $_GET) ? true : false;
+		$mode = array_key_exists('mode', $_GET) ? $_GET['mode'] : 'fill';
+
+		$preview = new \OC\Preview();
+		$preview->setFile($this->getInternalPath(), $this->getFileInfo());
+		$preview->setMaxX($maxX);
+		$preview->setMaxY($maxY);
+		$preview->setScalingUp($scalingUp);
+		$preview->setMode($mode);
+		$preview->setKeepAspect($keepAspect);
+		return $preview->getPreview();
 	}
 }

lib/private/Preview/TXT.php
@@ -43,8 +43,8 @@ class TXT extends Provider {
 	/**
 	 * {@inheritDoc}
 	 */
-	public function getThumbnail($path, $maxX, $maxY, $scalingup, $fileview) {
-		$content = $fileview->fopen($path, 'r');
+	public function getThumbnail($file, $maxX, $maxY, $scalingup) {
+		$content = $file->fopen('r');
 		$content = stream_get_contents($content,3000);
 
 		//don't create previews of empty text files

BWTECH-github/owncloud.online

lib/private/Files/Node/File.php
@@ -26,9 +26,11 @@
 
 namespace OC\Files\Node;
 
+use OCP\Files\IPreviewNode;
 use OCP\Files\NotPermittedException;
+use OCP\IImage;
 
-class File extends Node implements \OCP\Files\File {
+class File extends Node implements \OCP\Files\File, IPreviewNode {
 	/**
 	 * Creates a Folder that represents a non-existing path
 	 *
@@ -137,5 +139,27 @@ class File extends Node implements \OCP\Files\File {
 	 */
 	public function getChecksum() {
 		return $this->getFileInfo()->getChecksum();
+	}
+
+	/**
+	 * @param array $options
+	 * @return IImage
+	 * @since 10.1.0
+	 */
+	public function getThumbnail($options) {
+		$maxX = array_key_exists('x', $options) ? (int)$options['x'] : 32;
+		$maxY = array_key_exists('y', $options) ? (int)$options['y'] : 32;
+		$scalingUp = array_key_exists('scalingup', $options) ? (bool)$options['scalingup'] : true;
+		$keepAspect = array_key_exists('a', $options) ? true : false;
+		$mode = array_key_exists('mode', $options) ? $options['mode'] : 'fill';
+
+		$preview = new \OC\Preview();
+		$preview->setFile($this->getInternalPath(), $this->getFileInfo());
+		$preview->setMaxX($maxX);
+		$preview->setMaxY($maxY);
+		$preview->setScalingUp($scalingUp);
+		$preview->setMode($mode);
+		$preview->setKeepAspect($keepAspect);
+		return $preview->getPreview();
 	}
 }

lib/private/Preview/TXT.php
@@ -43,8 +43,8 @@ class TXT extends Provider {
 	/**
 	 * {@inheritDoc}
 	 */
-	public function getThumbnail($path, $maxX, $maxY, $scalingup, $fileview) {
-		$stream = $fileview->fopen($path, 'r');
+	public function getThumbnail($file, $maxX, $maxY, $scalingup) {
+		$stream = $file->fopen('r');
 		$content = stream_get_contents($stream,3000);
 		fclose($stream);
 