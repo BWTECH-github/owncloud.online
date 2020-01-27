visense/core

Split trash manager and trash expiry manager, optimize trash expiry for memory

apps/files_trashbin/lib/Command/ExpireTrash.php
@@ -22,9 +22,7 @@
 
 namespace OCA\Files_Trashbin\Command;
 
-use OCA\Files_Trashbin\Expiration;
-use OCA\Files_Trashbin\Helper;
-use OCA\Files_Trashbin\Trashbin;
+use OCA\Files_Trashbin\TrashExpiryManager;
 use OCP\IUser;
 use OCP\IUserManager;
 use Symfony\Component\Console\Command\Command;
@@ -36,9 +34,9 @@ use Symfony\Component\Console\Output\OutputInterface;
 class ExpireTrash extends Command {
 
 	/**
-	 * @var Expiration
+	 * @var TrashExpiryManager
 	 */
-	private $expiration;
+	private $trashExpiryManager;
 	
 	/**
 	 * @var IUserManager
@@ -47,14 +45,14 @@ class ExpireTrash extends Command {
 
 	/**
 	 * @param IUserManager|null $userManager
-	 * @param Expiration|null $expiration
+	 * @param TrashExpiryManager|null $trashExpiryManager
 	 */
 	public function __construct(IUserManager $userManager = null,
-								Expiration $expiration = null) {
+								TrashExpiryManager $trashExpiryManager = null) {
 		parent::__construct();
 
 		$this->userManager = $userManager;
-		$this->expiration = $expiration;
+		$this->trashExpiryManager = $trashExpiryManager;
 	}
 
 	protected function configure() {
@@ -69,8 +67,8 @@ class ExpireTrash extends Command {
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output) {
-		$maxAge = $this->expiration->getMaxAgeAsTimestamp();
-		if (!$maxAge) {
+		$retentionEnabled = $this->trashExpiryManager->expiryByRetentionEnabled();
+		if (!$retentionEnabled) {
 			$output->writeln("Auto expiration is configured - expiration will be handled automatically.");
 			return;
 		}
@@ -103,8 +101,8 @@ class ExpireTrash extends Command {
 		if (!$this->setupFS($uid)) {
 			return;
 		}
-		$dirContent = Helper::getTrashFiles('/', $uid, 'mtime');
-		Trashbin::deleteExpiredFiles($dirContent, $uid);
+
+		$this->trashExpiryManager->expireTrashByRetention($uid);
 	}
 
 	/**

BWTECH-github/owncloud.online

apps/files_trashbin/lib/Command/ExpireTrash.php
@@ -22,9 +22,7 @@
 
 namespace OCA\Files_Trashbin\Command;
 
-use OCA\Files_Trashbin\Expiration;
-use OCA\Files_Trashbin\Helper;
-use OCA\Files_Trashbin\Trashbin;
+use OCA\Files_Trashbin\TrashExpiryManager;
 use OCP\IUser;
 use OCP\IUserManager;
 use Symfony\Component\Console\Command\Command;
@@ -36,9 +34,9 @@ use Symfony\Component\Console\Output\OutputInterface;
 class ExpireTrash extends Command {
 
 	/**
-	 * @var Expiration
+	 * @var TrashExpiryManager
 	 */
-	private $expiration;
+	private $trashExpiryManager;
 	
 	/**
 	 * @var IUserManager
@@ -47,14 +45,14 @@ class ExpireTrash extends Command {
 
 	/**
 	 * @param IUserManager|null $userManager
-	 * @param Expiration|null $expiration
+	 * @param TrashExpiryManager|null $trashExpiryManager
 	 */
 	public function __construct(IUserManager $userManager = null,
-								Expiration $expiration = null) {
+								TrashExpiryManager $trashExpiryManager = null) {
 		parent::__construct();
 
 		$this->userManager = $userManager;
-		$this->expiration = $expiration;
+		$this->trashExpiryManager = $trashExpiryManager;
 	}
 
 	protected function configure() {
@@ -69,8 +67,8 @@ class ExpireTrash extends Command {
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output) {
-		$maxAge = $this->expiration->getMaxAgeAsTimestamp();
-		if (!$maxAge) {
+		$retentionEnabled = $this->trashExpiryManager->retentionEnabled();
+		if (!$retentionEnabled) {
 			$output->writeln("Auto expiration is configured - expiration will be handled automatically.");
 			return;
 		}
@@ -103,8 +101,8 @@ class ExpireTrash extends Command {
 		if (!$this->setupFS($uid)) {
 			return;
 		}
-		$dirContent = Helper::getTrashFiles('/', $uid, 'mtime');
-		Trashbin::deleteExpiredFiles($dirContent, $uid);
+
+		$this->trashExpiryManager->expireTrashByRetention($uid);
 	}
 
 	/**