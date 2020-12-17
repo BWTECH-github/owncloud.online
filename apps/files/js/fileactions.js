saw-jan/core

Add a context menu to choose a file action if there is more then one default action

apps/files/js/fileactions.js
@@ -215,34 +215,24 @@
 		 * @param {string} type "dir" or "file"
 		 * @param {int} permissions permissions
 		 *
-		 * @return {Array.<OCA.Files.FileAction>} array of action specs
+		 * @return {Object.<OCA.Files.FileAction>} array of action specs
 		 */
 		getActions: function (mime, type, permissions) {
-			var actions = {};
-			if (this.actions.all) {
-				actions = $.extend(actions, this.actions.all);
-			}
-			if (type) {//type is 'dir' or 'file'
-				if (this.actions[type]) {
-					actions = $.extend(actions, this.actions[type]);
-				}
-			}
-			if (mime) {
-				var mimePart = mime.substr(0, mime.indexOf('/'));
-				if (this.actions[mimePart]) {
-					actions = $.extend(actions, this.actions[mimePart]);
-				}
-				if (this.actions[mime]) {
-					actions = $.extend(actions, this.actions[mime]);
-				}
-			}
-			var filteredActions = {};
-			$.each(actions, function (name, action) {
-				if (action.permissions & permissions) {
-					filteredActions[name] = action;
-				}
-			});
-			return filteredActions;
+			return this._getActions(mime, type, permissions, false);
+		},
+
+		/**
+		 * Returns an array of file actions matching the given conditions while
+		 * omitting the default actions like "Rename", "Download" etc.
+		 *
+		 * @param {string} mime mime type
+		 * @param {string} type "dir" or "file"
+		 * @param {int} permissions permissions
+		 *
+		 * @return {Object.<OCA.Files.FileAction>} array of action specs
+		 */
+		getActionsWithoutDefaults: function (mime, type, permissions) {
+			return this._getActions(mime, type, permissions, true);
 		},
 
 		/**
@@ -291,6 +281,45 @@
 			}
 			var actions = this.getActions(mime, type, permissions);
 			return actions[name];
+		},
+
+		/**
+		 * Returns an array of file actions matching the given conditions
+		 *
+		 * @param {string} mime mime type
+		 * @param {string} type "dir" or "file"
+		 * @param {int} permissions permissions
+		 * @param {boolean} excludeDefaults excludeDefaults
+		 *
+		 * @return {Object.<OCA.Files.FileAction>} array of action specs
+		 */
+		_getActions: function (mime, type, permissions, excludeDefaults) {
+			var actions = {};
+
+			if (this.actions.all && excludeDefaults !== true) {
+				actions = $.extend(actions, this.actions.all);
+			}
+			if (type) {//type is 'dir' or 'file'
+				if (this.actions[type]) {
+					actions = $.extend(actions, this.actions[type]);
+				}
+			}
+			if (mime) {
+				var mimePart = mime.substr(0, mime.indexOf('/'));
+				if (this.actions[mimePart]) {
+					actions = $.extend(actions, this.actions[mimePart]);
+				}
+				if (this.actions[mime]) {
+					actions = $.extend(actions, this.actions[mime]);
+				}
+			}
+			var filteredActions = {};
+			$.each(actions, function (name, action) {
+				if (action.permissions & permissions) {
+					filteredActions[name] = action;
+				}
+			});
+			return filteredActions;
 		},
 
 		/**
@@ -736,6 +765,7 @@
 	 * @property {Object} $file jQuery file row element
 	 * @property {OCA.Files.FileActions} fileActions file actions object
 	 * @property {OCA.Files.FileList} fileList file list object
+	 * @property {String} dir file dir
 	 */
 
 	/**

apps/files/js/filelist.js
@@ -665,17 +665,28 @@
 						var mime = this.fileActions.getCurrentMimeType();
 						var type = this.fileActions.getCurrentType();
 						var permissions = this.fileActions.getCurrentPermissions();
+						var actionsWithoutDefaults = this.fileActions.getActionsWithoutDefaults(mime,type, permissions);
+						var context = {
+							$file: $tr,
+							fileList: this,
+							fileActions: this.fileActions,
+							dir: $tr.attr('data-path') || this.getCurrentDirectory()
+						};
+
+						// don't show app drawer for directories as we want to open them per default
+						if (Object.keys(actionsWithoutDefaults).length > 1 && type !== 'dir') {
+							var appDrawer = new OCA.Files.FileActionsAppDrawer();
+							appDrawer.show(context, $tr.find('td.filename'));
+							event.preventDefault();
+							return;
+						}
+
 						var action = this.fileActions.getDefault(mime,type, permissions);
 						if (action) {
 							event.preventDefault();
 							// also set on global object for legacy apps
 							window.FileActions.currentFile = this.fileActions.currentFile;
-							action(filename, {
-								$file: $tr,
-								fileList: this,
-								fileActions: this.fileActions,
-								dir: $tr.attr('data-path') || this.getCurrentDirectory()
-							});
+							action(filename, context);
 						}
 						// deselect row
 						$(event.target).closest('a').blur();

apps/files/lib/Controller/ViewController.php
@@ -186,6 +186,7 @@ class ViewController extends Controller {
 
 		\OCP\Util::addScript('files', 'fileactions');
 		\OCP\Util::addScript('files', 'fileactionsmenu');
+		\OCP\Util::addScript('files', 'fileactionsappdrawer');
 		\OCP\Util::addScript('files', 'files');
 		\OCP\Util::addScript('files', 'keyboardshortcuts');
 		\OCP\Util::addScript('files', 'navigation');

BWTECH-github/owncloud.online

apps/files/js/fileactions.js
@@ -215,34 +215,24 @@
 		 * @param {string} type "dir" or "file"
 		 * @param {int} permissions permissions
 		 *
-		 * @return {Array.<OCA.Files.FileAction>} array of action specs
+		 * @return {Object.<OCA.Files.FileAction>} array of action specs
 		 */
 		getActions: function (mime, type, permissions) {
-			var actions = {};
-			if (this.actions.all) {
-				actions = $.extend(actions, this.actions.all);
-			}
-			if (type) {//type is 'dir' or 'file'
-				if (this.actions[type]) {
-					actions = $.extend(actions, this.actions[type]);
-				}
-			}
-			if (mime) {
-				var mimePart = mime.substr(0, mime.indexOf('/'));
-				if (this.actions[mimePart]) {
-					actions = $.extend(actions, this.actions[mimePart]);
-				}
-				if (this.actions[mime]) {
-					actions = $.extend(actions, this.actions[mime]);
-				}
-			}
-			var filteredActions = {};
-			$.each(actions, function (name, action) {
-				if (action.permissions & permissions) {
-					filteredActions[name] = action;
-				}
-			});
-			return filteredActions;
+			return this._getActions(mime, type, permissions, false);
+		},
+
+		/**
+		 * Returns an array of file actions matching the given conditions while
+		 * omitting the actions which apply to all mime types ("Rename", "Download" etc.)
+		 *
+		 * @param {string} mime mime type
+		 * @param {string} type "dir" or "file"
+		 * @param {int} permissions permissions
+		 *
+		 * @return {Object.<OCA.Files.FileAction>} array of action specs
+		 */
+		getActionsWithoutAll: function (mime, type, permissions) {
+			return this._getActions(mime, type, permissions, true);
 		},
 
 		/**
@@ -291,6 +281,45 @@
 			}
 			var actions = this.getActions(mime, type, permissions);
 			return actions[name];
+		},
+
+		/**
+		 * Returns an array of file actions matching the given conditions
+		 *
+		 * @param {string} mime mime type
+		 * @param {string} type "dir" or "file"
+		 * @param {int} permissions permissions
+		 * @param {boolean} excludeAll excludeAll
+		 *
+		 * @return {Object.<OCA.Files.FileAction>} array of action specs
+		 */
+		_getActions: function (mime, type, permissions, excludeAll) {
+			var actions = {};
+
+			if (this.actions.all && excludeAll !== true) {
+				actions = $.extend(actions, this.actions.all);
+			}
+			if (type) {//type is 'dir' or 'file'
+				if (this.actions[type]) {
+					actions = $.extend(actions, this.actions[type]);
+				}
+			}
+			if (mime) {
+				var mimePart = mime.substr(0, mime.indexOf('/'));
+				if (this.actions[mimePart]) {
+					actions = $.extend(actions, this.actions[mimePart]);
+				}
+				if (this.actions[mime]) {
+					actions = $.extend(actions, this.actions[mime]);
+				}
+			}
+			var filteredActions = {};
+			$.each(actions, function (name, action) {
+				if (action.permissions & permissions) {
+					filteredActions[name] = action;
+				}
+			});
+			return filteredActions;
 		},
 
 		/**
@@ -736,6 +765,7 @@
 	 * @property {Object} $file jQuery file row element
 	 * @property {OCA.Files.FileActions} fileActions file actions object
 	 * @property {OCA.Files.FileList} fileList file list object
+	 * @property {String} dir file dir
 	 */
 
 	/**

apps/files/js/filelist.js
@@ -665,17 +665,28 @@
 						var mime = this.fileActions.getCurrentMimeType();
 						var type = this.fileActions.getCurrentType();
 						var permissions = this.fileActions.getCurrentPermissions();
+						var actionsWithoutAll = this.fileActions.getActionsWithoutAll(mime,type, permissions);
+						var context = {
+							$file: $tr,
+							fileList: this,
+							fileActions: this.fileActions,
+							dir: $tr.attr('data-path') || this.getCurrentDirectory()
+						};
+
+						// don't show app drawer for directories as we want to open them per default
+						if (Object.keys(actionsWithoutAll).length > 1 && type !== 'dir') {
+							var appSelectMenu = new OCA.Files.FileActionsApplicationSelectMenu();
+							appSelectMenu.show(context, $tr.find('td.filename'));
+							event.preventDefault();
+							return;
+						}
+
 						var action = this.fileActions.getDefault(mime,type, permissions);
 						if (action) {
 							event.preventDefault();
 							// also set on global object for legacy apps
 							window.FileActions.currentFile = this.fileActions.currentFile;
-							action(filename, {
-								$file: $tr,
-								fileList: this,
-								fileActions: this.fileActions,
-								dir: $tr.attr('data-path') || this.getCurrentDirectory()
-							});
+							action(filename, context);
 						}
 						// deselect row
 						$(event.target).closest('a').blur();

apps/files/lib/Controller/ViewController.php
@@ -186,6 +186,7 @@ class ViewController extends Controller {
 
 		\OCP\Util::addScript('files', 'fileactions');
 		\OCP\Util::addScript('files', 'fileactionsmenu');
+		\OCP\Util::addScript('files', 'fileactionsapplicationselectmenu');
 		\OCP\Util::addScript('files', 'files');
 		\OCP\Util::addScript('files', 'keyboardshortcuts');
 		\OCP\Util::addScript('files', 'navigation');