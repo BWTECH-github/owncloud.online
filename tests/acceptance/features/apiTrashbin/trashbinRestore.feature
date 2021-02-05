Site-Command/core

expand test coverage for hidden file

tests/acceptance/features/apiTrashbin/trashbinRestore.feature
@@ -337,3 +337,25 @@ Feature: Restore deleted files/folders
       | dav-path |
       | old      |
       | new      |
+
+  @smokeTest
+  Scenario Outline: A deleted hidden file can be restored
+    Given using <dav-path> DAV path
+    And user "Alice" has created folder "/FOLDER"
+    And user "Alice" has uploaded file with content "to delete" to "<path>"
+    And user "Alice" has deleted file "<path>"
+    And as "Alice" file "<path>" should not exist
+    But as "Alice" the file with original path "<path>" should exist in the trashbin
+    When user "Alice" restores the folder with original path "<path>" using the trashbin API
+    Then the HTTP status code should be "201"
+    And the following headers should match these regular expressions for user "Alice"
+      | ETag | /^"[a-f0-9:\.]{1,32}"$/ |
+    And as "Alice" the file with original path "<path>" should not exist in the trashbin
+    And as "Alice" file "<path>" should exist
+    Then the content of file "<path>" for user "Alice" should be "to delete"
+    Examples:
+      | dav-path | path                 |
+      | old      | .hidden_file         |
+      | old      | /FOLDER/.hidden_file |
+      | new      | .hidden_file         |
+      | new      | /FOLDER/.hidden_file |

tests/acceptance/features/apiWebdavUpload1/uploadFile.feature
@@ -199,3 +199,18 @@ Feature: upload file
       | dav_version |
       | old         |
       | new         |
+
+  @smokeTest
+  Scenario Outline: upload a hidden file and check download content
+    Given using <dav_version> DAV path
+    And user "Alice" has created folder "/FOLDER"
+    When user "Alice" uploads file with content "uploaded content" to "<path>" using the WebDAV API
+    Then the following headers should match these regular expressions for user "Alice"
+      | ETag | /^"[a-f0-9:\.]{1,32}"$/ |
+    Then the content of file "<path>" for user "Alice" should be "uploaded content"
+    Examples:
+      | dav_version | path                 |
+      | old         | .hidden_file         |
+      | old         | /FOLDER/.hidden_file |
+      | new         | .hidden_file         |
+      | new         | /FOLDER/.hidden_file |

BWTECH-github/owncloud.online

tests/acceptance/features/apiTrashbin/trashbinRestore.feature
@@ -337,3 +337,23 @@ Feature: Restore deleted files/folders
       | dav-path |
       | old      |
       | new      |
+
+  @smokeTest
+  Scenario Outline: A deleted hidden file can be restored
+    Given using <dav-path> DAV path
+    And user "Alice" has created folder "/FOLDER"
+    And user "Alice" has uploaded file with content "to delete" to "<path>"
+    And user "Alice" has deleted file "<path>"
+    When user "Alice" restores the folder with original path "<path>" using the trashbin API
+    Then the HTTP status code should be "201"
+    And the following headers should match these regular expressions for user "Alice"
+      | ETag | /^"[a-f0-9:\.]{1,32}"$/ |
+    And as "Alice" the file with original path "<path>" should not exist in the trashbin
+    And as "Alice" file "<path>" should exist
+    And the content of file "<path>" for user "Alice" should be "to delete"
+    Examples:
+      | dav-path | path                 |
+      | old      | .hidden_file         |
+      | old      | /FOLDER/.hidden_file |
+      | new      | .hidden_file         |
+      | new      | /FOLDER/.hidden_file |

tests/acceptance/features/apiWebdavUpload1/uploadFile.feature
@@ -199,3 +199,18 @@ Feature: upload file
       | dav_version |
       | old         |
       | new         |
+
+  @smokeTest
+  Scenario Outline: upload a hidden file and check download content
+    Given using <dav_version> DAV path
+    And user "Alice" has created folder "/FOLDER"
+    When user "Alice" uploads file with content "uploaded content" to "<path>" using the WebDAV API
+    Then the following headers should match these regular expressions for user "Alice"
+      | ETag | /^"[a-f0-9:\.]{1,32}"$/ |
+    And the content of file "<path>" for user "Alice" should be "uploaded content"
+    Examples:
+      | dav_version | path                 |
+      | old         | .hidden_file         |
+      | old         | /FOLDER/.hidden_file |
+      | new         | .hidden_file         |
+      | new         | /FOLDER/.hidden_file |