Georgeajosw/owncloud

add group action steps

tests/acceptance/features/apiWebdavMove2/moveFile.feature
@@ -335,34 +335,40 @@ Feature: move (rename) file
       | old         |
       | new         |
 
-  @smokeTest
   Scenario Outline: Moving a hidden file
     Given using <dav_version> DAV path
-    And user "Alice" has uploaded file with content "I am a hidden file" to "<file_name_from>"
-    When user "Alice" moves file "<file_name_from>" to "<file_name_to>" using the WebDAV API
-    Then the HTTP status code should be "201"
-    And the following headers should match these regular expressions for user "Alice"
-      | ETag | /^"[a-f0-9:\.]{1,32}"$/ |
-    And the content of file "<file_name_to>" for user "Alice" should be "I am a hidden file"
+    And user "Alice" has uploaded the following files with content "hidden file"
+      | path                    |
+      | .hidden_file101         |
+      | /FOLDER/.hidden_file102 |
+    When user "Alice" moves the following files using the WebDAV API
+      | from                    | to                      |
+      | .hidden_file101         | /FOLDER/.hidden_file101 |
+      | /FOLDER/.hidden_file102 | .hidden_file102         |
+    Then as "Alice" the following files should exist
+      | path                    |
+      | .hidden_file102         |
+      | /FOLDER/.hidden_file101 |
     Examples:
-      | dav_version | file_name_from         | file_name_to          |
-      | old         | .hidden_file           | /FOLDER/.hidden_file  |
-      | old         | /FOLDER/.hidden_file   | .hidden_file          |
-      | new         | .hidden_file           | /FOLDER/.hidden_file  |
-      | new         | /FOLDER/.hidden_file   | .hidden_file          |
+      | dav_version |
+      | old         |
+      | new         |
 
-  @smokeTest
   Scenario Outline: Renaming to/from a hidden file
     Given using <dav_version> DAV path
-    And user "Alice" has uploaded file with content "I am a hidden file" to "<file_name_from>"
-    When user "Alice" moves file "<file_name_from>" to "<file_name_to>" using the WebDAV API
-    Then the HTTP status code should be "201"
-    And the following headers should match these regular expressions for user "Alice"
-      | ETag | /^"[a-f0-9:\.]{1,32}"$/ |
-    And the content of file "<file_name_to>" for user "Alice" should be "I am a hidden file"
+    And user "Alice" has uploaded the following files with content "hidden file"
+      | path                    |
+      | .hidden_file101         |
+      | hidden_file101.txt      |
+    When user "Alice" moves the following files using the WebDAV API
+      | from               | to                 |
+      | .hidden_file101    | hidden_file102.txt |
+      | hidden_file101.txt | .hidden_file102    |
+    Then as "Alice" the following files should exist
+      | path               |
+      | .hidden_file102    |
+      | hidden_file102.txt |
     Examples:
-      | dav_version | file_name_from  | file_name_to    |
-      | old         | .hidden_file    | hidden_file.txt |
-      | old         | hidden_file.txt | .hidden_file    |
-      | new         | .hidden_file    | hidden_file.txt |
-      | new         | hidden_file.txt | .hidden_file    |
+      | dav_version |
+      | old         |
+      | new         |

tests/acceptance/features/apiWebdavOperations/deleteFile.feature
@@ -70,17 +70,22 @@ Feature: delete file
       | new         | ",,,.txt"      |
       | new         | ",,,.,"        |
 
-  @smokeTest
   Scenario Outline: delete a hidden file
     Given using <dav_version> DAV path
     And user "Alice" has created folder "/FOLDER"
-    And user "Alice" has uploaded file with content "to delete" to "<path>"
-    When user "Alice" deletes file "<path>" using the WebDAV API
-    Then the HTTP status code should be "204"
-    And as "Alice" file "<path>" should not exist
+    And user "Alice" has uploaded the following files with content "hidden file"
+      | path                 |
+      | .hidden_file         |
+      | /FOLDER/.hidden_file |
+    When user "Alice" deletes the following files
+      | path                 |
+      | .hidden_file         |
+      | /FOLDER/.hidden_file |
+    Then as "Alice" the following files should not exist
+      | path                 |
+      | .hidden_file         |
+      | /FOLDER/.hidden_file |
     Examples:
-      | dav_version | path                 |
-      | old         | .hidden_file         |
-      | old         | /FOLDER/.hidden_file |
-      | new         | .hidden_file         |
-      | new         | /FOLDER/.hidden_file |
+      | dav_version |
+      | old         |
+      | new         |

tests/acceptance/features/apiWebdavOperations/downloadFile.feature
@@ -198,16 +198,18 @@ Feature: download file
       | old         |
       | new         |
 
-  @smokeTest
   Scenario Outline: download a hidden file
     Given using <dav_version> DAV path
     And user "Alice" has created folder "/FOLDER"
-    And user "Alice" has uploaded file with content "I am a hidden file" to "<path>"
-    When user "Alice" downloads file "<path>" using the WebDAV API
-    Then the downloaded content should be "I am a hidden file"
+    And user "Alice" has uploaded the following files with content "hidden file"
+      | path                 |
+      | .hidden_file         |
+      | /FOLDER/.hidden_file |
+    When user "Alice" downloads file ".hidden_file" using the WebDAV API
+    Then the downloaded content should be "hidden file"
+    When user "Alice" downloads file "./FOLDER/.hidden_file" using the WebDAV API
+    Then the downloaded content should be "hidden file"
     Examples:
-      | dav_version | path                 |
-      | old         | .hidden_file         |
-      | old         | /FOLDER/.hidden_file |
-      | new         | .hidden_file         |
-      | new         | /FOLDER/.hidden_file |
+      | dav_version |
+      | old         |
+      | new         |

tests/acceptance/features/apiWebdavUpload1/uploadFile.feature
@@ -200,17 +200,22 @@ Feature: upload file
       | old         |
       | new         |
 
-  @smokeTest
   Scenario Outline: upload a hidden file and check download content
     Given using <dav_version> DAV path
     And user "Alice" has created folder "/FOLDER"
-    When user "Alice" uploads file with content "uploaded content" to "<path>" using the WebDAV API
-    Then the following headers should match these regular expressions for user "Alice"
-      | ETag | /^"[a-f0-9:\.]{1,32}"$/ |
-    And the content of file "<path>" for user "Alice" should be "uploaded content"
+    When user "Alice" uploads the following files with content "hidden file"
+      | path                 |
+      | .hidden_file         |
+      | /FOLDER/.hidden_file |
+    Then as "Alice" the following files should exist
+      | path                 |
+      | .hidden_file         |
+      | /FOLDER/.hidden_file |
+    And the content of the following files for user "Alice" should be "hidden file"
+      | path                 |
+      | .hidden_file         |
+      | /FOLDER/.hidden_file |
     Examples:
-      | dav_version | path                 |
-      | old         | .hidden_file         |
-      | old         | /FOLDER/.hidden_file |
-      | new         | .hidden_file         |
-      | new         | /FOLDER/.hidden_file |
+      | dav_version |
+      | old         |
+      | new         |

BWTECH-github/owncloud.online

tests/acceptance/features/apiWebdavMove2/moveFile.feature
@@ -335,34 +335,42 @@ Feature: move (rename) file
       | old         |
       | new         |
 
-  @smokeTest
   Scenario Outline: Moving a hidden file
     Given using <dav_version> DAV path
-    And user "Alice" has uploaded file with content "I am a hidden file" to "<file_name_from>"
-    When user "Alice" moves file "<file_name_from>" to "<file_name_to>" using the WebDAV API
-    Then the HTTP status code should be "201"
-    And the following headers should match these regular expressions for user "Alice"
-      | ETag | /^"[a-f0-9:\.]{1,32}"$/ |
-    And the content of file "<file_name_to>" for user "Alice" should be "I am a hidden file"
+    And user "Alice" has uploaded the following files with content "hidden file"
+      | path                    |
+      | .hidden_file101         |
+      | /FOLDER/.hidden_file102 |
+    When user "Alice" moves the following files using the WebDAV API
+      | from                    | to                      |
+      | .hidden_file101         | /FOLDER/.hidden_file101 |
+      | /FOLDER/.hidden_file102 | .hidden_file102         |
+    Then the HTTP status code of responses on all endpoints should be "201"
+    And as "Alice" the following files should exist
+      | path                    |
+      | .hidden_file102         |
+      | /FOLDER/.hidden_file101 |
     Examples:
-      | dav_version | file_name_from         | file_name_to          |
-      | old         | .hidden_file           | /FOLDER/.hidden_file  |
-      | old         | /FOLDER/.hidden_file   | .hidden_file          |
-      | new         | .hidden_file           | /FOLDER/.hidden_file  |
-      | new         | /FOLDER/.hidden_file   | .hidden_file          |
+      | dav_version |
+      | old         |
+      | new         |
 
-  @smokeTest
   Scenario Outline: Renaming to/from a hidden file
     Given using <dav_version> DAV path
-    And user "Alice" has uploaded file with content "I am a hidden file" to "<file_name_from>"
-    When user "Alice" moves file "<file_name_from>" to "<file_name_to>" using the WebDAV API
-    Then the HTTP status code should be "201"
-    And the following headers should match these regular expressions for user "Alice"
-      | ETag | /^"[a-f0-9:\.]{1,32}"$/ |
-    And the content of file "<file_name_to>" for user "Alice" should be "I am a hidden file"
+    And user "Alice" has uploaded the following files with content "hidden file"
+      | path                    |
+      | .hidden_file101         |
+      | hidden_file101.txt      |
+    When user "Alice" moves the following files using the WebDAV API
+      | from               | to                 |
+      | .hidden_file101    | hidden_file102.txt |
+      | hidden_file101.txt | .hidden_file102    |
+    Then the HTTP status code of responses on all endpoints should be "201"
+    And as "Alice" the following files should exist
+      | path               |
+      | .hidden_file102    |
+      | hidden_file102.txt |
     Examples:
-      | dav_version | file_name_from  | file_name_to    |
-      | old         | .hidden_file    | hidden_file.txt |
-      | old         | hidden_file.txt | .hidden_file    |
-      | new         | .hidden_file    | hidden_file.txt |
-      | new         | hidden_file.txt | .hidden_file    |
+      | dav_version |
+      | old         |
+      | new         |

tests/acceptance/features/apiWebdavOperations/deleteFile.feature
@@ -70,17 +70,23 @@ Feature: delete file
       | new         | ",,,.txt"      |
       | new         | ",,,.,"        |
 
-  @smokeTest
   Scenario Outline: delete a hidden file
     Given using <dav_version> DAV path
     And user "Alice" has created folder "/FOLDER"
-    And user "Alice" has uploaded file with content "to delete" to "<path>"
-    When user "Alice" deletes file "<path>" using the WebDAV API
-    Then the HTTP status code should be "204"
-    And as "Alice" file "<path>" should not exist
+    And user "Alice" has uploaded the following files with content "hidden file"
+      | path                 |
+      | .hidden_file         |
+      | /FOLDER/.hidden_file |
+    When user "Alice" deletes the following files
+      | path                 |
+      | .hidden_file         |
+      | /FOLDER/.hidden_file |
+    Then the HTTP status code of responses on all endpoints should be "204"
+    And as "Alice" the following files should not exist
+      | path                 |
+      | .hidden_file         |
+      | /FOLDER/.hidden_file |
     Examples:
-      | dav_version | path                 |
-      | old         | .hidden_file         |
-      | old         | /FOLDER/.hidden_file |
-      | new         | .hidden_file         |
-      | new         | /FOLDER/.hidden_file |
+      | dav_version |
+      | old         |
+      | new         |

tests/acceptance/features/apiWebdavOperations/downloadFile.feature
@@ -198,16 +198,20 @@ Feature: download file
       | old         |
       | new         |
 
-  @smokeTest
   Scenario Outline: download a hidden file
     Given using <dav_version> DAV path
     And user "Alice" has created folder "/FOLDER"
-    And user "Alice" has uploaded file with content "I am a hidden file" to "<path>"
-    When user "Alice" downloads file "<path>" using the WebDAV API
-    Then the downloaded content should be "I am a hidden file"
+    And user "Alice" has uploaded the following files with content "hidden file"
+      | path                 |
+      | .hidden_file         |
+      | /FOLDER/.hidden_file |
+    When user "Alice" downloads file ".hidden_file" using the WebDAV API
+    Then the HTTP status code should be "200"
+    And the downloaded content should be "hidden file"
+    When user "Alice" downloads file "./FOLDER/.hidden_file" using the WebDAV API
+    Then the HTTP status code should be "200"
+    And the downloaded content should be "hidden file"
     Examples:
-      | dav_version | path                 |
-      | old         | .hidden_file         |
-      | old         | /FOLDER/.hidden_file |
-      | new         | .hidden_file         |
-      | new         | /FOLDER/.hidden_file |
+      | dav_version |
+      | old         |
+      | new         |

tests/acceptance/features/apiWebdavUpload1/uploadFile.feature
@@ -200,17 +200,23 @@ Feature: upload file
       | old         |
       | new         |
 
-  @smokeTest
   Scenario Outline: upload a hidden file and check download content
     Given using <dav_version> DAV path
     And user "Alice" has created folder "/FOLDER"
-    When user "Alice" uploads file with content "uploaded content" to "<path>" using the WebDAV API
-    Then the following headers should match these regular expressions for user "Alice"
-      | ETag | /^"[a-f0-9:\.]{1,32}"$/ |
-    And the content of file "<path>" for user "Alice" should be "uploaded content"
+    When user "Alice" uploads the following files with content "hidden file"
+      | path                 |
+      | .hidden_file         |
+      | /FOLDER/.hidden_file |
+    Then the HTTP status code of responses on all endpoints should be "201"
+    And as "Alice" the following files should exist
+      | path                 |
+      | .hidden_file         |
+      | /FOLDER/.hidden_file |
+    And the content of the following files for user "Alice" should be "hidden file"
+      | path                 |
+      | .hidden_file         |
+      | /FOLDER/.hidden_file |
     Examples:
-      | dav_version | path                 |
-      | old         | .hidden_file         |
-      | old         | /FOLDER/.hidden_file |
-      | new         | .hidden_file         |
-      | new         | /FOLDER/.hidden_file |
+      | dav_version |
+      | old         |
+      | new         |