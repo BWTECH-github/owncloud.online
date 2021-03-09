laradevio/core

[Tests-Only] Refactor api scenario outline examples (part2)

tests/acceptance/features/apiProvisioningGroups-v1/deleteGroupOc10Issue31015.feature
@@ -8,22 +8,36 @@ Feature: delete groups
     Given using OCS API version "1"
 
   @issue-31015
-  Scenario Outline: admin deletes a group that has a forward-slash in the group name
+  Scenario: admin deletes a group that has a forward-slash in the group name
     # After fixing issue-31015, change the following step to "has been created"
-    Given the administrator sends a group creation request for group "<group_id>" using the provisioning API
-    #Given group "<group_id>" has been created
-    When the administrator deletes group "<group_id>" using the provisioning API
-    # After fixing issue-31015, change the expected status to "100"
-    #Then the OCS status code should be "100"
-    And the HTTP status code should be "404"
-    #And the HTTP status code should be "200"
-    And group "<group_id>" should not exist
-    # The following step is needed so that the group does get cleaned up.
-    # After fixing issue-31015, remove the following step:
-    And the administrator deletes group "<group_id>" using the occ command
-    Examples:
-      | group_id         | comment                            |
+    When the administrator sends a group creation request for the following group using the provisioning API
+      | groupname        | comment                            |
       | Mgmt/Sydney      | Slash (special escaping happens)   |
       | Mgmt//NSW/Sydney | Multiple slash                     |
       | var/../etc       | using slash-dot-dot                |
       | priv/subadmins/1 | Subadmins mentioned not at the end |
+    #Given group "<group_id>" has been created
+    When the administrator deletes the following groups using the provisioning API
+      | groupname        |
+      | Mgmt/Sydney      |
+      | Mgmt//NSW/Sydney |
+      | var/../etc       |
+      | priv/subadmins/1 |
+    # After fixing issue-31015, change the expected status to "100"
+    #Then the OCS status code should be "100"
+    Then the HTTP status code of responses on all endpoints should be "404"
+    #And the HTTP status code should be "200"
+    And the following groups should not exist
+      | groupname        |
+      | Mgmt/Sydney      |
+      | Mgmt//NSW/Sydney |
+      | var/../etc       |
+      | priv/subadmins/1 |
+    # The following step is needed so that the group does get cleaned up.
+    # After fixing issue-31015, remove the following step:
+    And the administrator deletes the following groups using the occ command
+      | groupname        |
+      | Mgmt/Sydney      |
+      | Mgmt//NSW/Sydney |
+      | var/../etc       |
+      | priv/subadmins/1 |

tests/acceptance/features/apiProvisioningGroups-v2/addGroup.feature
@@ -8,24 +8,23 @@ Feature: add groups
     Given using OCS API version "2"
 
   @smokeTest
-  Scenario Outline: admin creates a group
-    When the administrator sends a group creation request for group "<group_id>" using the provisioning API
-    Then the OCS status code should be "200"
-    And the HTTP status code should be "200"
-    And group "<group_id>" should exist
-    Examples:
-      | group_id    | comment                               |
+  Scenario: admin creates a group
+    When the administrator sends a group creation request for the following group using the provisioning API
+      | groupname   | comment                               |
       | simplegroup | nothing special here                  |
       | España§àôœ€ | special European and other characters |
-      | नेपाली      | Unicode group name                    |
+      | नेपाली        | Unicode group name                    |
+    Then the OCS status code of responses on all endpoints should be "200"
+    And the HTTP status code of responses on all endpoints should be "200"
+    And these groups should exist:
+      | groupname   |
+      | simplegroup |
+      | España§àôœ€ |
+      | नेपाली        |
 
-  Scenario Outline: admin creates a group
-    When the administrator sends a group creation request for group "<group_id>" using the provisioning API
-    Then the OCS status code should be "200"
-    And the HTTP status code should be "200"
-    And group "<group_id>" should exist
-    Examples:
-      | group_id            | comment                                 |
+  Scenario: admin creates a group
+    When the administrator sends a group creation request for the following group using the provisioning API
+      | groupname           | comment                                 |
       | brand-new-group     | dash                                    |
       | the.group           | dot                                     |
       | left,right          | comma                                   |
@@ -37,49 +36,85 @@ Feature: add groups
       | $x<=>[y*z^2]!       | Maths symbols                           |
       | Mgmt\Middle         | Backslash                               |
       | 😅 😆               | emoji                                   |
+    Then the OCS status code of responses on all endpoints should be "200"
+    And the HTTP status code of responses on all endpoints should be "200"
+    And these groups should exist:
+      | groupname           |
+      | brand-new-group     |
+      | the.group           |
+      | left,right          |
+      | 0                   |
+      | Finance (NP)        |
+      | Admin&Finance       |
+      | admin:Pokhara@Nepal |
+      | maint+eng           |
+      | $x<=>[y*z^2]!       |
+      | Mgmt\Middle         |
+      | 😅 😆               |
 
   @toImplementOnOCIS @issue-product-284
-  Scenario Outline: admin creates a group
-    When the administrator sends a group creation request for group "<group_id>" using the provisioning API
-    Then the OCS status code should be "200"
-    And the HTTP status code should be "200"
-    And group "<group_id>" should exist
-    Examples:
-      | group_id            | comment                                 |
+  Scenario: admin creates a group
+    When the administrator sends a group creation request for the following group using the provisioning API
+      | groupname           | comment                                 |
       | maintenance#123     | Hash sign                               |
       | 50%pass             | Percent sign (special escaping happens) |
       | 50%25=0             | %25 literal looks like an escaped "%"   |
       | 50%2Fix             | %2F literal looks like an escaped slash |
       | 50%2Eagle           | %2E literal looks like an escaped "."   |
       | staff?group         | Question mark                           |
+    Then the OCS status code of responses on all endpoints should be "200"
+    And the HTTP status code of responses on all endpoints should be "200"
+    And these groups should exist:
+      | groupname           |
+      | maintenance#123     |
+      | 50%pass             |
+      | 50%25=0             |
+      | 50%2Fix             |
+      | 50%2Eagle           |
+      | staff?group         |
 
   @toImplementOnOCIS
-  Scenario Outline: group names are case-sensitive, multiple groups can exist with different upper and lower case names
-    When the administrator sends a group creation request for group "<group_id1>" using the provisioning API
-    And the administrator sends a group creation request for group "<group_id2>" using the provisioning API
-    Then the OCS status code should be "200"
-    And the HTTP status code should be "200"
-    And group "<group_id1>" should exist
-    And group "<group_id2>" should exist
-    But group "<group_id3>" should not exist
-    Examples:
-      | group_id1            | group_id2            | group_id3            |
-      | case-sensitive-group | Case-Sensitive-Group | CASE-SENSITIVE-GROUP |
-      | Case-Sensitive-Group | CASE-SENSITIVE-GROUP | case-sensitive-group |
-      | CASE-SENSITIVE-GROUP | case-sensitive-group | Case-Sensitive-Group |
+  Scenario: group names are case-sensitive, multiple groups can exist with different upper and lower case names
+    When the administrator sends a group creation request for the following group using the provisioning API
+      | groupname             |
+      | Case-Sensitive-Group1 |
+      | case-sensitive-group1 |
+      | Case-Sensitive-Group2 |
+      | CASE-SENSITIVE-GROUP2 |
+      | case-sensitive-group3 |
+      | CASE-SENSITIVE-GROUP3 |
+    Then the OCS status code of responses on all endpoints should be "200"
+    And the HTTP status code of responses on all endpoints should be "200"
+    And these groups should exist:
+      | groupname             |
+      | Case-Sensitive-Group1 |
+      | case-sensitive-group1 |
+      | Case-Sensitive-Group2 |
+      | CASE-SENSITIVE-GROUP2 |
+      | case-sensitive-group3 |
+      | CASE-SENSITIVE-GROUP3 |
+    And these groups should not exist:
+      | groupname             |
+      | CASE-SENSITIVE-GROUP1 |
+      | case-sensitive-group2 |
+      | Case-Sensitive-Group3 |
 
   @issue-31015 @skipOnOcV10 @toImplementOnOCIS
-  Scenario Outline: admin creates a group with a forward-slash in the group name
-    When the administrator sends a group creation request for group "<group_id>" using the provisioning API
-    Then the OCS status code should be "200"
-    And the HTTP status code should be "200"
-    And group "<group_id>" should exist
-    Examples:
-      | group_id         | comment                            |
+  Scenario: admin creates a group with a forward-slash in the group name
+    When the administrator sends a group creation request for the following group using the provisioning API
+      | groupname        | comment                            |
       | Mgmt/Sydney      | Slash (special escaping happens)   |
       | Mgmt//NSW/Sydney | Multiple slash                     |
       | var/../etc       | using slash-dot-dot                |
       | priv/subadmins/1 | Subadmins mentioned not at the end |
+    Then the OCS status code of responses on all endpoints should be "100"
+    And the HTTP status code of responses on all endpoints should be "200"
+    And these groups should exist:
+      | groupname        |
+      | Mgmt/Sydney      |
+      | Mgmt//NSW/Sydney |
+      | var/../etc       |
+      | priv/subadmins/1 |
 
   # A group name must not end in "/subadmins" because that would create ambiguity
   # with the endpoint for getting the subadmins of a group

BWTECH-github/owncloud.online

tests/acceptance/features/apiProvisioningGroups-v1/deleteGroupOc10Issue31015.feature
@@ -8,22 +8,35 @@ Feature: delete groups
     Given using OCS API version "1"
 
   @issue-31015
-  Scenario Outline: admin deletes a group that has a forward-slash in the group name
+  Scenario: admin deletes a group that has a forward-slash in the group name
     # After fixing issue-31015, change the following step to "has been created"
-    Given the administrator sends a group creation request for group "<group_id>" using the provisioning API
-    #Given group "<group_id>" has been created
-    When the administrator deletes group "<group_id>" using the provisioning API
-    # After fixing issue-31015, change the expected status to "100"
-    #Then the OCS status code should be "100"
-    And the HTTP status code should be "404"
-    #And the HTTP status code should be "200"
-    And group "<group_id>" should not exist
-    # The following step is needed so that the group does get cleaned up.
-    # After fixing issue-31015, remove the following step:
-    And the administrator deletes group "<group_id>" using the occ command
-    Examples:
-      | group_id         | comment                            |
+    Given the administrator sends a group creation request for the following groups using the provisioning API
+      | groupname        | comment                            |
       | Mgmt/Sydney      | Slash (special escaping happens)   |
       | Mgmt//NSW/Sydney | Multiple slash                     |
       | var/../etc       | using slash-dot-dot                |
       | priv/subadmins/1 | Subadmins mentioned not at the end |
+    When the administrator deletes the following groups using the provisioning API
+      | groupname        |
+      | Mgmt/Sydney      |
+      | Mgmt//NSW/Sydney |
+      | var/../etc       |
+      | priv/subadmins/1 |
+    # After fixing issue-31015, change the expected status to "100"
+    #Then the OCS status code should be "100"
+    Then the HTTP status code of responses on all endpoints should be "404"
+    #And the HTTP status code should be "200"
+    And the following groups should not exist
+      | groupname        |
+      | Mgmt/Sydney      |
+      | Mgmt//NSW/Sydney |
+      | var/../etc       |
+      | priv/subadmins/1 |
+    # The following step is needed so that the group does get cleaned up.
+    # After fixing issue-31015, remove the following step:
+    And the administrator deletes the following groups using the occ command
+      | groupname        |
+      | Mgmt/Sydney      |
+      | Mgmt//NSW/Sydney |
+      | var/../etc       |
+      | priv/subadmins/1 |

tests/acceptance/features/apiProvisioningGroups-v2/addGroup.feature
@@ -8,24 +8,23 @@ Feature: add groups
     Given using OCS API version "2"
 
   @smokeTest
-  Scenario Outline: admin creates a group
-    When the administrator sends a group creation request for group "<group_id>" using the provisioning API
-    Then the OCS status code should be "200"
-    And the HTTP status code should be "200"
-    And group "<group_id>" should exist
-    Examples:
-      | group_id    | comment                               |
+  Scenario: admin creates a group
+    When the administrator sends a group creation request for the following groups using the provisioning API
+      | groupname   | comment                               |
       | simplegroup | nothing special here                  |
       | España§àôœ€ | special European and other characters |
-      | नेपाली      | Unicode group name                    |
+      | नेपाली        | Unicode group name                    |
+    Then the OCS status code of responses on all endpoints should be "200"
+    And the HTTP status code of responses on all endpoints should be "200"
+    And these groups should exist:
+      | groupname   |
+      | simplegroup |
+      | España§àôœ€ |
+      | नेपाली        |
 
-  Scenario Outline: admin creates a group
-    When the administrator sends a group creation request for group "<group_id>" using the provisioning API
-    Then the OCS status code should be "200"
-    And the HTTP status code should be "200"
-    And group "<group_id>" should exist
-    Examples:
-      | group_id            | comment                                 |
+  Scenario: admin creates a group
+    When the administrator sends a group creation request for the following groups using the provisioning API
+      | groupname           | comment                                 |
       | brand-new-group     | dash                                    |
       | the.group           | dot                                     |
       | left,right          | comma                                   |
@@ -37,49 +36,85 @@ Feature: add groups
       | $x<=>[y*z^2]!       | Maths symbols                           |
       | Mgmt\Middle         | Backslash                               |
       | 😅 😆               | emoji                                   |
+    Then the OCS status code of responses on all endpoints should be "200"
+    And the HTTP status code of responses on all endpoints should be "200"
+    And these groups should exist:
+      | groupname           |
+      | brand-new-group     |
+      | the.group           |
+      | left,right          |
+      | 0                   |
+      | Finance (NP)        |
+      | Admin&Finance       |
+      | admin:Pokhara@Nepal |
+      | maint+eng           |
+      | $x<=>[y*z^2]!       |
+      | Mgmt\Middle         |
+      | 😅 😆               |
 
   @toImplementOnOCIS @issue-product-284
-  Scenario Outline: admin creates a group
-    When the administrator sends a group creation request for group "<group_id>" using the provisioning API
-    Then the OCS status code should be "200"
-    And the HTTP status code should be "200"
-    And group "<group_id>" should exist
-    Examples:
-      | group_id            | comment                                 |
+  Scenario: admin creates a group
+    When the administrator sends a group creation request for the following groups using the provisioning API
+      | groupname           | comment                                 |
       | maintenance#123     | Hash sign                               |
       | 50%pass             | Percent sign (special escaping happens) |
       | 50%25=0             | %25 literal looks like an escaped "%"   |
       | 50%2Fix             | %2F literal looks like an escaped slash |
       | 50%2Eagle           | %2E literal looks like an escaped "."   |
       | staff?group         | Question mark                           |
+    Then the OCS status code of responses on all endpoints should be "200"
+    And the HTTP status code of responses on all endpoints should be "200"
+    And these groups should exist:
+      | groupname           |
+      | maintenance#123     |
+      | 50%pass             |
+      | 50%25=0             |
+      | 50%2Fix             |
+      | 50%2Eagle           |
+      | staff?group         |
 
   @toImplementOnOCIS
-  Scenario Outline: group names are case-sensitive, multiple groups can exist with different upper and lower case names
-    When the administrator sends a group creation request for group "<group_id1>" using the provisioning API
-    And the administrator sends a group creation request for group "<group_id2>" using the provisioning API
-    Then the OCS status code should be "200"
-    And the HTTP status code should be "200"
-    And group "<group_id1>" should exist
-    And group "<group_id2>" should exist
-    But group "<group_id3>" should not exist
-    Examples:
-      | group_id1            | group_id2            | group_id3            |
-      | case-sensitive-group | Case-Sensitive-Group | CASE-SENSITIVE-GROUP |
-      | Case-Sensitive-Group | CASE-SENSITIVE-GROUP | case-sensitive-group |
-      | CASE-SENSITIVE-GROUP | case-sensitive-group | Case-Sensitive-Group |
+  Scenario: group names are case-sensitive, multiple groups can exist with different upper and lower case names
+    When the administrator sends a group creation request for the following groups using the provisioning API
+      | groupname             |
+      | Case-Sensitive-Group1 |
+      | case-sensitive-group1 |
+      | Case-Sensitive-Group2 |
+      | CASE-SENSITIVE-GROUP2 |
+      | case-sensitive-group3 |
+      | CASE-SENSITIVE-GROUP3 |
+    Then the OCS status code of responses on all endpoints should be "200"
+    And the HTTP status code of responses on all endpoints should be "200"
+    And these groups should exist:
+      | groupname             |
+      | Case-Sensitive-Group1 |
+      | case-sensitive-group1 |
+      | Case-Sensitive-Group2 |
+      | CASE-SENSITIVE-GROUP2 |
+      | case-sensitive-group3 |
+      | CASE-SENSITIVE-GROUP3 |
+    And these groups should not exist:
+      | groupname             |
+      | CASE-SENSITIVE-GROUP1 |
+      | case-sensitive-group2 |
+      | Case-Sensitive-Group3 |
 
   @issue-31015 @skipOnOcV10 @toImplementOnOCIS
-  Scenario Outline: admin creates a group with a forward-slash in the group name
-    When the administrator sends a group creation request for group "<group_id>" using the provisioning API
-    Then the OCS status code should be "200"
-    And the HTTP status code should be "200"
-    And group "<group_id>" should exist
-    Examples:
-      | group_id         | comment                            |
+  Scenario: admin creates a group with a forward-slash in the group name
+    When the administrator sends a group creation request for the following groups using the provisioning API
+      | groupname        | comment                            |
       | Mgmt/Sydney      | Slash (special escaping happens)   |
       | Mgmt//NSW/Sydney | Multiple slash                     |
       | var/../etc       | using slash-dot-dot                |
       | priv/subadmins/1 | Subadmins mentioned not at the end |
+    Then the OCS status code of responses on all endpoints should be "100"
+    And the HTTP status code of responses on all endpoints should be "200"
+    And these groups should exist:
+      | groupname        |
+      | Mgmt/Sydney      |
+      | Mgmt//NSW/Sydney |
+      | var/../etc       |
+      | priv/subadmins/1 |
 
   # A group name must not end in "/subadmins" because that would create ambiguity
   # with the endpoint for getting the subadmins of a group