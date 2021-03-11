laradevio/core

[Tests-Only] Refactor api scenario outline examples (part3)

tests/acceptance/features/apiProvisioningGroups-v1/removeFromGroupOc10Issue31015.feature
@@ -8,22 +8,41 @@ Feature: remove a user from a group
     Given using OCS API version "1"
 
   @issue-31015
-  Scenario Outline: admin removes a user from a group that has a forward-slash in the group name
+  Scenario: admin removes a user from a group that has a forward-slash in the group name
     Given user "brand-new-user" has been created with default attributes and small skeleton files
     # After fixing issue-31015, change the following step to "has been created"
-    And the administrator sends a group creation request for group "<group_id>" using the provisioning API
-    #And group "<group_id>" has been created
-    And user "brand-new-user" has been added to group "<group_id>"
-    When the administrator removes user "brand-new-user" from group "<group_id>" using the provisioning API
-    Then the OCS status code should be "100"
-    And the HTTP status code should be "200"
-    And user "brand-new-user" should not belong to group "<group_id>"
-    # The following step is needed so that the group does get cleaned up.
-    # After fixing issue-31015, remove the following step:
-    And the administrator deletes group "<group_id>" using the occ command
-    Examples:
-      | group_id         | comment                            |
+    And the administrator sends a group creation request for the following group using the provisioning API
+      | groupname        | comment                            |
       | Mgmt/Sydney      | Slash (special escaping happens)   |
       | Mgmt//NSW/Sydney | Multiple slash                     |
       | var/../etc       | using slash-dot-dot                |
       | priv/subadmins/1 | Subadmins mentioned not at the end |
+    #And group "<group_id>" has been created
+    And the following user have been added to the following group
+      | username       | groupname        |
+      | brand-new-user | Mgmt/Sydney      |
+      | brand-new-user | Mgmt//NSW/Sydney |
+      | brand-new-user | var/../etc       |
+      | brand-new-user | priv/subadmins/1 |
+    When the administrator removes the following user from the following group using the provisioning API
+      | username       | groupname        |
+      | brand-new-user | Mgmt/Sydney      |
+      | brand-new-user | Mgmt//NSW/Sydney |
+      | brand-new-user | var/../etc       |
+      | brand-new-user | priv/subadmins/1 |
+    Then the OCS status code of responses on all endpoints should be "100"
+    And the HTTP status code of responses on all endpoints should be "200"
+    And the the following user should not belong to the following group
+      | username       | groupname        |
+      | brand-new-user | Mgmt/Sydney      |
+      | brand-new-user | Mgmt//NSW/Sydney |
+      | brand-new-user | var/../etc       |
+      | brand-new-user | priv/subadmins/1 |
+    # The following step is needed so that the group does get cleaned up.
+    # After fixing issue-31015, remove the following step:
+    And the administrator deletes the following groups using the occ command
+      | groupname        |
+      | Mgmt/Sydney      |
+      | Mgmt//NSW/Sydney |
+      | var/../etc       |
+      | priv/subadmins/1 |

tests/acceptance/features/apiProvisioningGroups-v2/addGroupOc10Issue31015.feature
@@ -12,23 +12,32 @@ Feature: add groups
     # 2) the ordinary group deletion in AfterScenario does not work, because the
     #    group-delete API does not work for groups with a slash in the name
   @issue-31015
-  Scenario Outline: admin creates a group with a forward-slash in the group name
-    When the administrator sends a group creation request for group "<group_id>" using the provisioning API
-    Then the OCS status code should be "200"
-    And the HTTP status code should be "200"
-    # After fixing issue-31015, change the following step to "should exist"
-    And group "<group_id>" should not exist
-    #And group "<group_id>" should exist
-    #
-    # The following step is needed so that the group does get cleaned up.
-    # After fixing issue-31015, remove the following step:
-    And the administrator deletes group "<group_id>" using the occ command
-    Examples:
-      | group_id         | comment                            |
+  Scenario: admin creates a group with a forward-slash in the group name
+    When the administrator sends a group creation request for the following group using the provisioning API
+      | groupname        | comment                            |
       | Mgmt/Sydney      | Slash (special escaping happens)   |
       | Mgmt//NSW/Sydney | Multiple slash                     |
       | var/../etc       | using slash-dot-dot                |
       | priv/subadmins/1 | Subadmins mentioned not at the end |
+    Then the OCS status code of responses on all endpoints should be "200"
+    And the HTTP status code of responses on all endpoints should be "200"
+    # After fixing issue-31015, change the following step to "should exist"
+    And the following groups should not exist
+      | groupname        |
+      | Mgmt/Sydney      |
+      | Mgmt//NSW/Sydney |
+      | var/../etc       |
+      | priv/subadmins/1 |
+    #And group "<group_id>" should exist
+    #
+    # The following step is needed so that the group does get cleaned up.
+    # After fixing issue-31015, remove the following step:
+    And the administrator deletes the following groups using the occ command
+      | groupname        |
+      | Mgmt/Sydney      |
+      | Mgmt//NSW/Sydney |
+      | var/../etc       |
+      | priv/subadmins/1 |
 
   # A group name must not end in "/subadmins" because that would create ambiguity
   # with the endpoint for getting the subadmins of a group

tests/acceptance/features/apiProvisioningGroups-v2/addToGroupOc10Issue31015.feature
@@ -8,20 +8,29 @@ Feature: add users to group
     Given using OCS API version "2"
 
   @issue-31015 @skipOnLDAP
-  Scenario Outline: adding a user to a group that has a forward-slash in the group name
+  Scenario: adding a user to a group that has a forward-slash in the group name
     Given user "brand-new-user" has been created with default attributes and small skeleton files
     # After fixing issue-31015, change the following step to "has been created"
-    And the administrator sends a group creation request for group "<group_id>" using the provisioning API
-    #And group "<group_id>" has been created
-    When the administrator adds user "brand-new-user" to group "<group_id>" using the provisioning API
-    Then the OCS status code should be "200"
-    And the HTTP status code should be "200"
-    # The following step is needed so that the group does get cleaned up.
-    # After fixing issue-31015, remove the following step:
-    And the administrator deletes group "<group_id>" using the occ command
-    Examples:
-      | group_id         | comment                            |
+    When the administrator sends a group creation request for the following group using the provisioning API
+      | groupname        | comment                            |
       | Mgmt/Sydney      | Slash (special escaping happens)   |
       | Mgmt//NSW/Sydney | Multiple slash                     |
       | var/../etc       | using slash-dot-dot                |
       | priv/subadmins/1 | Subadmins mentioned not at the end |
+    #And group "<group_id>" has been created
+    And the administrator adds the following users to the following groups using the provisioning API
+      | username       | groupname        |
+      | brand-new-user | Mgmt/Sydney      |
+      | brand-new-user | Mgmt//NSW/Sydney |
+      | brand-new-user | var/../etc       |
+      | brand-new-user | priv/subadmins/1 |
+    Then the OCS status code of responses on all endpoints should be "200"
+    And the HTTP status code of responses on all endpoints should be "200"
+    # The following step is needed so that the group does get cleaned up.
+    # After fixing issue-31015, remove the following step:
+    And the administrator deletes the following groups using the occ command
+      | groupname        |
+      | Mgmt/Sydney      |
+      | Mgmt//NSW/Sydney |
+      | var/../etc       |
+      | priv/subadmins/1 |

tests/acceptance/features/apiProvisioningGroups-v2/deleteGroupOc10Issue31015.feature
@@ -8,22 +8,36 @@ Feature: delete groups
     Given using OCS API version "2"
 
   @issue-31015
-  Scenario Outline: admin deletes a group that has a forward-slash in the group name
+  Scenario: admin deletes a group that has a forward-slash in the group name
     # After fixing issue-31015, change the following step to "has been created"
-    Given the administrator sends a group creation request for group "<group_id>" using the provisioning API
-    #Given group "<group_id>" has been created
-    When the administrator deletes group "<group_id>" using the provisioning API
-    # After fixing issue-31015, change the expected status to "200"
-    #Then the OCS status code should be "200"
-    And the HTTP status code should be "404"
-    #And the HTTP status code should be "200"
-    And group "<group_id>" should not exist
-    # The following step is needed so that the group does get cleaned up.
-    # After fixing issue-31015, remove the following step:
-    And the administrator deletes group "<group_id>" using the occ command
-    Examples:
-      | group_id         | comment                            |
+    When the administrator sends a group creation request for the following group using the provisioning API
+      | groupname        | comment                            |
       | Mgmt/Sydney      | Slash (special escaping happens)   |
       | Mgmt//NSW/Sydney | Multiple slash                     |
       | var/../etc       | using slash-dot-dot                |
       | priv/subadmins/1 | Subadmins mentioned not at the end |
+    #Given group "<group_id>" has been created
+    And the administrator deletes the following groups using the provisioning API
+      | groupname        |
+      | Mgmt/Sydney      |
+      | Mgmt//NSW/Sydney |
+      | var/../etc       |
+      | priv/subadmins/1 |
+    # After fixing issue-31015, change the expected status to "200"
+    #Then the OCS status code should be "200"
+    Then the HTTP status code of responses on all endpoints should be "404"
+    #And the HTTP status code should be "200"
+    And the following groups should not exist
+      | groupname        |
+      | Mgmt/Sydney      |
+      | Mgmt//NSW/Sydney |
+      | var/../etc       |
+      | priv/subadmins/1 |
+    # The following step is needed so that the group does get cleaned up.
+    # After fixing issue-31015, remove the following step:
+    And the administrator deletes the following groups using the occ command
+      | groupname        |
+      | Mgmt/Sydney      |
+      | Mgmt//NSW/Sydney |
+      | var/../etc       |
+      | priv/subadmins/1 |

tests/acceptance/features/apiProvisioningGroups-v2/removeFromGroupOc10Issue31015.feature
@@ -11,19 +11,38 @@ Feature: remove a user from a group
   Scenario Outline: admin removes a user from a group that has a forward-slash in the group name
     Given user "brand-new-user" has been created with default attributes and small skeleton files
       # After fixing issue-31015, change the following step to "has been created"
-    And the administrator sends a group creation request for group "<group_id>" using the provisioning API
-    #And group "<group_id>" has been created
-    And user "brand-new-user" has been added to group "<group_id>"
-    When the administrator removes user "brand-new-user" from group "<group_id>" using the provisioning API
-    Then the OCS status code should be "200"
-    And the HTTP status code should be "200"
-    And user "brand-new-user" should not belong to group "<group_id>"
-    # The following step is needed so that the group does get cleaned up.
-    # After fixing issue-31015, remove the following step:
-    And the administrator deletes group "<group_id>" using the occ command
-    Examples:
-      | group_id         | comment                            |
+    And the administrator sends a group creation request for the following group using the provisioning API
+      | groupname        | comment                            |
       | Mgmt/Sydney      | Slash (special escaping happens)   |
       | Mgmt//NSW/Sydney | Multiple slash                     |
       | var/../etc       | using slash-dot-dot                |
       | priv/subadmins/1 | Subadmins mentioned not at the end |
+    #And group "<group_id>" has been created
+    And the following user have been added to the following group
+      | username       | groupname        |
+      | brand-new-user | Mgmt/Sydney      |
+      | brand-new-user | Mgmt//NSW/Sydney |
+      | brand-new-user | var/../etc       |
+      | brand-new-user | priv/subadmins/1 |
+    When the administrator removes the following user from the following group using the provisioning API
+      | username       | groupname        |
+      | brand-new-user | Mgmt/Sydney      |
+      | brand-new-user | Mgmt//NSW/Sydney |
+      | brand-new-user | var/../etc       |
+      | brand-new-user | priv/subadmins/1 |
+    Then the OCS status code of responses on all endpoints should be "200"
+    And the HTTP status code of responses on all endpoints should be "200"
+    And the the following user should not belong to the following group
+      | username       | groupname        |
+      | brand-new-user | Mgmt/Sydney      |
+      | brand-new-user | Mgmt//NSW/Sydney |
+      | brand-new-user | var/../etc       |
+      | brand-new-user | priv/subadmins/1 |
+    # The following step is needed so that the group does get cleaned up.
+    # After fixing issue-31015, remove the following step:
+    And the administrator deletes the following groups using the occ command
+      | groupname        |
+      | Mgmt/Sydney      |
+      | Mgmt//NSW/Sydney |
+      | var/../etc       |
+      | priv/subadmins/1 |

BWTECH-github/owncloud.online

tests/acceptance/features/apiProvisioningGroups-v1/removeFromGroupOc10Issue31015.feature
@@ -8,22 +8,41 @@ Feature: remove a user from a group
     Given using OCS API version "1"
 
   @issue-31015
-  Scenario Outline: admin removes a user from a group that has a forward-slash in the group name
+  Scenario: admin removes a user from a group that has a forward-slash in the group name
     Given user "brand-new-user" has been created with default attributes and small skeleton files
     # After fixing issue-31015, change the following step to "has been created"
-    And the administrator sends a group creation request for group "<group_id>" using the provisioning API
-    #And group "<group_id>" has been created
-    And user "brand-new-user" has been added to group "<group_id>"
-    When the administrator removes user "brand-new-user" from group "<group_id>" using the provisioning API
-    Then the OCS status code should be "100"
-    And the HTTP status code should be "200"
-    And user "brand-new-user" should not belong to group "<group_id>"
-    # The following step is needed so that the group does get cleaned up.
-    # After fixing issue-31015, remove the following step:
-    And the administrator deletes group "<group_id>" using the occ command
-    Examples:
-      | group_id         | comment                            |
+    And the administrator sends a group creation request for the following groups using the provisioning API
+      | groupname        | comment                            |
       | Mgmt/Sydney      | Slash (special escaping happens)   |
       | Mgmt//NSW/Sydney | Multiple slash                     |
       | var/../etc       | using slash-dot-dot                |
       | priv/subadmins/1 | Subadmins mentioned not at the end |
+    #And group "<group_id>" has been created
+    And the following users have been added to the following groups
+      | username       | groupname        |
+      | brand-new-user | Mgmt/Sydney      |
+      | brand-new-user | Mgmt//NSW/Sydney |
+      | brand-new-user | var/../etc       |
+      | brand-new-user | priv/subadmins/1 |
+    When the administrator removes the following users from the following groups using the provisioning API
+      | username       | groupname        |
+      | brand-new-user | Mgmt/Sydney      |
+      | brand-new-user | Mgmt//NSW/Sydney |
+      | brand-new-user | var/../etc       |
+      | brand-new-user | priv/subadmins/1 |
+    Then the OCS status code of responses on all endpoints should be "100"
+    And the HTTP status code of responses on all endpoints should be "200"
+    And the following users should not belong to the following groups
+      | username       | groupname        |
+      | brand-new-user | Mgmt/Sydney      |
+      | brand-new-user | Mgmt//NSW/Sydney |
+      | brand-new-user | var/../etc       |
+      | brand-new-user | priv/subadmins/1 |
+    # The following step is needed so that the group does get cleaned up.
+    # After fixing issue-31015, remove the following step:
+    And the administrator deletes the following groups using the occ command
+      | groupname        |
+      | Mgmt/Sydney      |
+      | Mgmt//NSW/Sydney |
+      | var/../etc       |
+      | priv/subadmins/1 |

tests/acceptance/features/apiProvisioningGroups-v2/addGroupOc10Issue31015.feature
@@ -12,23 +12,32 @@ Feature: add groups
     # 2) the ordinary group deletion in AfterScenario does not work, because the
     #    group-delete API does not work for groups with a slash in the name
   @issue-31015
-  Scenario Outline: admin creates a group with a forward-slash in the group name
-    When the administrator sends a group creation request for group "<group_id>" using the provisioning API
-    Then the OCS status code should be "200"
-    And the HTTP status code should be "200"
-    # After fixing issue-31015, change the following step to "should exist"
-    And group "<group_id>" should not exist
-    #And group "<group_id>" should exist
-    #
-    # The following step is needed so that the group does get cleaned up.
-    # After fixing issue-31015, remove the following step:
-    And the administrator deletes group "<group_id>" using the occ command
-    Examples:
-      | group_id         | comment                            |
+  Scenario: admin creates a group with a forward-slash in the group name
+    When the administrator sends a group creation request for the following groups using the provisioning API
+      | groupname        | comment                            |
       | Mgmt/Sydney      | Slash (special escaping happens)   |
       | Mgmt//NSW/Sydney | Multiple slash                     |
       | var/../etc       | using slash-dot-dot                |
       | priv/subadmins/1 | Subadmins mentioned not at the end |
+    Then the OCS status code of responses on all endpoints should be "200"
+    And the HTTP status code of responses on all endpoints should be "200"
+    # After fixing issue-31015, change the following step to "should exist"
+    And the following groups should not exist
+      | groupname        |
+      | Mgmt/Sydney      |
+      | Mgmt//NSW/Sydney |
+      | var/../etc       |
+      | priv/subadmins/1 |
+    #And group "<group_id>" should exist
+    #
+    # The following step is needed so that the group does get cleaned up.
+    # After fixing issue-31015, remove the following step:
+    And the administrator deletes the following groups using the occ command
+      | groupname        |
+      | Mgmt/Sydney      |
+      | Mgmt//NSW/Sydney |
+      | var/../etc       |
+      | priv/subadmins/1 |
 
   # A group name must not end in "/subadmins" because that would create ambiguity
   # with the endpoint for getting the subadmins of a group

tests/acceptance/features/apiProvisioningGroups-v2/addToGroupOc10Issue31015.feature
@@ -8,20 +8,29 @@ Feature: add users to group
     Given using OCS API version "2"
 
   @issue-31015 @skipOnLDAP
-  Scenario Outline: adding a user to a group that has a forward-slash in the group name
+  Scenario: adding a user to a group that has a forward-slash in the group name
     Given user "brand-new-user" has been created with default attributes and small skeleton files
     # After fixing issue-31015, change the following step to "has been created"
-    And the administrator sends a group creation request for group "<group_id>" using the provisioning API
-    #And group "<group_id>" has been created
-    When the administrator adds user "brand-new-user" to group "<group_id>" using the provisioning API
-    Then the OCS status code should be "200"
-    And the HTTP status code should be "200"
-    # The following step is needed so that the group does get cleaned up.
-    # After fixing issue-31015, remove the following step:
-    And the administrator deletes group "<group_id>" using the occ command
-    Examples:
-      | group_id         | comment                            |
+    When the administrator sends a group creation request for the following groups using the provisioning API
+      | groupname        | comment                            |
       | Mgmt/Sydney      | Slash (special escaping happens)   |
       | Mgmt//NSW/Sydney | Multiple slash                     |
       | var/../etc       | using slash-dot-dot                |
       | priv/subadmins/1 | Subadmins mentioned not at the end |
+    #And group "<group_id>" has been created
+    And the administrator adds the following users to the following groups using the provisioning API
+      | username       | groupname        |
+      | brand-new-user | Mgmt/Sydney      |
+      | brand-new-user | Mgmt//NSW/Sydney |
+      | brand-new-user | var/../etc       |
+      | brand-new-user | priv/subadmins/1 |
+    Then the OCS status code of responses on all endpoints should be "200"
+    And the HTTP status code of responses on all endpoints should be "200"
+    # The following step is needed so that the group does get cleaned up.
+    # After fixing issue-31015, remove the following step:
+    And the administrator deletes the following groups using the occ command
+      | groupname        |
+      | Mgmt/Sydney      |
+      | Mgmt//NSW/Sydney |
+      | var/../etc       |
+      | priv/subadmins/1 |

tests/acceptance/features/apiProvisioningGroups-v2/deleteGroupOc10Issue31015.feature
@@ -8,22 +8,36 @@ Feature: delete groups
     Given using OCS API version "2"
 
   @issue-31015
-  Scenario Outline: admin deletes a group that has a forward-slash in the group name
+  Scenario: admin deletes a group that has a forward-slash in the group name
     # After fixing issue-31015, change the following step to "has been created"
-    Given the administrator sends a group creation request for group "<group_id>" using the provisioning API
-    #Given group "<group_id>" has been created
-    When the administrator deletes group "<group_id>" using the provisioning API
-    # After fixing issue-31015, change the expected status to "200"
-    #Then the OCS status code should be "200"
-    And the HTTP status code should be "404"
-    #And the HTTP status code should be "200"
-    And group "<group_id>" should not exist
-    # The following step is needed so that the group does get cleaned up.
-    # After fixing issue-31015, remove the following step:
-    And the administrator deletes group "<group_id>" using the occ command
-    Examples:
-      | group_id         | comment                            |
+    When the administrator sends a group creation request for the following groups using the provisioning API
+      | groupname        | comment                            |
       | Mgmt/Sydney      | Slash (special escaping happens)   |
       | Mgmt//NSW/Sydney | Multiple slash                     |
       | var/../etc       | using slash-dot-dot                |
       | priv/subadmins/1 | Subadmins mentioned not at the end |
+    #Given group "<group_id>" has been created
+    And the administrator deletes the following groups using the provisioning API
+      | groupname        |
+      | Mgmt/Sydney      |
+      | Mgmt//NSW/Sydney |
+      | var/../etc       |
+      | priv/subadmins/1 |
+    # After fixing issue-31015, change the expected status to "200"
+    #Then the OCS status code should be "200"
+    Then the HTTP status code of responses on all endpoints should be "404"
+    #And the HTTP status code should be "200"
+    And the following groups should not exist
+      | groupname        |
+      | Mgmt/Sydney      |
+      | Mgmt//NSW/Sydney |
+      | var/../etc       |
+      | priv/subadmins/1 |
+    # The following step is needed so that the group does get cleaned up.
+    # After fixing issue-31015, remove the following step:
+    And the administrator deletes the following groups using the occ command
+      | groupname        |
+      | Mgmt/Sydney      |
+      | Mgmt//NSW/Sydney |
+      | var/../etc       |
+      | priv/subadmins/1 |

tests/acceptance/features/apiProvisioningGroups-v2/removeFromGroupOc10Issue31015.feature
@@ -8,22 +8,41 @@ Feature: remove a user from a group
     Given using OCS API version "2"
 
   @issue-31015
-  Scenario Outline: admin removes a user from a group that has a forward-slash in the group name
+  Scenario: admin removes a user from a group that has a forward-slash in the group name
     Given user "brand-new-user" has been created with default attributes and small skeleton files
       # After fixing issue-31015, change the following step to "has been created"
-    And the administrator sends a group creation request for group "<group_id>" using the provisioning API
-    #And group "<group_id>" has been created
-    And user "brand-new-user" has been added to group "<group_id>"
-    When the administrator removes user "brand-new-user" from group "<group_id>" using the provisioning API
-    Then the OCS status code should be "200"
-    And the HTTP status code should be "200"
-    And user "brand-new-user" should not belong to group "<group_id>"
-    # The following step is needed so that the group does get cleaned up.
-    # After fixing issue-31015, remove the following step:
-    And the administrator deletes group "<group_id>" using the occ command
-    Examples:
-      | group_id         | comment                            |
+    And the administrator sends a group creation request for the following groups using the provisioning API
+      | groupname        | comment                            |
       | Mgmt/Sydney      | Slash (special escaping happens)   |
       | Mgmt//NSW/Sydney | Multiple slash                     |
       | var/../etc       | using slash-dot-dot                |
       | priv/subadmins/1 | Subadmins mentioned not at the end |
+    #And group "<group_id>" has been created
+    And the following users have been added to the following groups
+      | username       | groupname        |
+      | brand-new-user | Mgmt/Sydney      |
+      | brand-new-user | Mgmt//NSW/Sydney |
+      | brand-new-user | var/../etc       |
+      | brand-new-user | priv/subadmins/1 |
+    When the administrator removes the following users from the following groups using the provisioning API
+      | username       | groupname        |
+      | brand-new-user | Mgmt/Sydney      |
+      | brand-new-user | Mgmt//NSW/Sydney |
+      | brand-new-user | var/../etc       |
+      | brand-new-user | priv/subadmins/1 |
+    Then the OCS status code of responses on all endpoints should be "200"
+    And the HTTP status code of responses on all endpoints should be "200"
+    And the following users should not belong to the following groups
+      | username       | groupname        |
+      | brand-new-user | Mgmt/Sydney      |
+      | brand-new-user | Mgmt//NSW/Sydney |
+      | brand-new-user | var/../etc       |
+      | brand-new-user | priv/subadmins/1 |
+    # The following step is needed so that the group does get cleaned up.
+    # After fixing issue-31015, remove the following step:
+    And the administrator deletes the following groups using the occ command
+      | groupname        |
+      | Mgmt/Sydney      |
+      | Mgmt//NSW/Sydney |
+      | var/../etc       |
+      | priv/subadmins/1 |