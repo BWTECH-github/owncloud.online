a01b03a54bacb1844a7942cfb54e8d3d586e5619
BWTECH-github <support@bw-tech.de>
2026-06-22 15:52:20 +0200

    release: ownCloud.online 11.0.7

--- version.php
+++ version.php
@@ -26,10 +26,10 @@
 // We only can count up. The 4. digit is only for the internal patch-level to trigger DB upgrades
 // between betas, final and RCs. This is _not_ the public version number. Reset minor/patch-level
 // when updating major/minor version number.
-$OC_Version = [11, 0, 6, 0];
+$OC_Version = [11, 0, 7, 0];
 
 // The human-readable string
-$OC_VersionString = '11.0.6';
+$OC_VersionString = '11.0.7';
 
 $OC_VersionCanBeUpgradedFrom = [[8, 2, 11],[9, 0, 9],[9, 1]];
 
