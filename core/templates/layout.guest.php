6518936cf3d869c12825e2d1768e4815e89ed118
f.boettcher <f.boettcher@bw.tech>
2026-08-11 14:16:41 +0200

    fix(a11y): Zoom-Sperre aus allen drei Seitenvorlagen entfernen [OC-WCAG-091, -092, -093]

--- core/templates/layout.guest.php
+++ core/templates/layout.guest.php
@@ -8,7 +8,7 @@
 		</title>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="referrer" content="never">
-		<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<?php if ($theme->getiTunesAppId() !== '') {
 			?>
 			<meta name="apple-itunes-app" content="app-id=<?php p($theme->getiTunesAppId()); ?>">
